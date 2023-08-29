f2bdcb9002b4a2ce74cfbfcbd6f9eca8219daa21
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2023-08-29 15:50:42 +0300

    Esimene tund

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());        // Avoid CORS errors in browsers
+app.use(express.json()) // Populate req.body
+
+const widgets = [
+    { id: 1, name: "Cizzbor", price: 29.99 },
+    { id: 2, name: "Woowo", price: 26.99 },
+    { id: 3, name: "Crazlinger", price: 59.99 },
+]
+
+app.get('/widgets', (req, res) => {
+    res.send(widgets)
+})
+
+app.get('/widgets/:id', (req, res) => {
+    const getIndex = widgets.findIndex(w => w.id === parseInt(req.params.id))
+    if (getIndex === -1) {
+        return res.status(404).send({ error: "Widget not found" })
+    }
+    res.send(widgets[getIndex])
+})
+
+app.post('/widgets', (req, res) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).send({ error: 'One or all params are missing' })
+    }
+    const newId = Math.max(...widgets.map((widget) => widget.id)) + 1
+    let newWidget = {
+        id: newId,
+        price: req.body.price,
+        name: req.body.name
+    }
+    widgets.push(newWidget)
+    res.status(201).location('localhost:8080/widgets/' + newId).send(
+        newWidget
+    )
+})
+
+app.delete('/widgets/:id', (req, res) => {
+    const deleteIndex = widgets.findIndex(w => w.id === parseInt(req.params.id))
+    if (deleteIndex === -1) {
+        return res.status(404).send({ error: "Widget not found" })
+    }
+    widgets.splice(deleteIndex, 1)
+    res.status(204).send()
+})
+
+
+app.listen(8080, () => {
+    console.log(`API up at: http://localhost:8080`)
+})
